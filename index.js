fd41d1b571756f3d8838662d3b3b7066d7bb84b9
brian corker <bcorker123@gmail.com>
2022-04-29 17:43:02 -0400

    commit tree 6062e65b8c6dbf282d6e99a632a4454c72ccf6e5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+// const element = document.createElement("div");
+// document.body.append(element)
+
+// const ul = document.createElement('ul');
+
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li');
+//     li.textContent = (i + 1);
+//     ul.append(li);
+// }
+
+// element.append(ul)
+
+
+const main = document.querySelector('#main')
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Brian is the champion'
+document.body.append(newHeader);
